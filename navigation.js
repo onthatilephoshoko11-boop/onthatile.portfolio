24f17eab3f20e80f6f4310c1a6de374acb9e558f
onthatilephoshoko11-boop <onthatilephoshoko11@gmail.com>
2026-06-30 18:08:02 +0200

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,15 +1,6 @@
-const hamburger = document.querySelector('.hamburger')
-const navlinks = document.querySelector('.nav-links')
-let menuOpen = false;
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener('click', () => {
-    if (!menuOpen == false) {
-        navlinks.style.display = "block";
-        menuOpen = true;
-    }
-    else if (menuOpen == true) {
-        navlinks.style.display = "none";
-        menuOpen = false;
-        
-    }
-})
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
